90d4cfbab3df77ecacdfc988705e018d529e904b
abusayadhussain <abusayadhussain@gmail.com>
2024-02-07 02:18:23 +0600

    unique email throws error fixed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,15 +2,18 @@ const User = require('../models/User');
 
 // Controller function to create a new user
 const createUser = async (req, res) => {
-  try {
-    const userData = req.body;
-    const newUser = await User.create(userData);
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+    try {
+      const userData = req.body;
+      const newUser = await User.create(userData);
+      res.status(201).json(newUser);
+    } catch (error) {
+      if (error.code === 11000 && error.keyPattern.email) { // Check for duplicate key error
+        return res.status(400).json({ error: 'Email already exists' });
+      }
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
 
 // Controller function to get all users
 const getAllUsers = async (req, res) => {
